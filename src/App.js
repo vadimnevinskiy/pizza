e29694ce5ad0da5d9309145fbd096754f6d0a95b
Vadim Nevinskiy <vadim_nevinskiy@mail.ru>
2021-05-10 17:11:33 +0500

    Sort component

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
                             onClickItem={(name) => console.log(name)}
                             items={['Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']}
                         />
-                        <Sort />
+                        <Sort items={['популярности', 'цене', 'алфавиту']} />
                     </div>
                     <h2 className="content__title">Все пиццы</h2>
                     <div className="content__items">
--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,9 +1,15 @@
 import React, {useEffect, useRef, useState} from 'react';
 
 
-const Sort = () => {
+const Sort = ({items}) => {
     const [showSort, setShowSort] = useState(false);
+    const [activeItem, setActiveItem] = useState(0);
     const sortRef = useRef(null);
+    const activeLabel = items[activeItem];
+
+    useEffect(() => {
+        document.body.addEventListener('click', handleOutsideClick)
+    }, [])
 
     const toggleSortPopup = () => {
         setShowSort(!showSort)
@@ -15,9 +21,10 @@ const Sort = () => {
         }
     }
 
-    useEffect(() => {
-        document.body.addEventListener('click', handleOutsideClick)
-    }, [])
+    const onSelectItem = (index) => {
+        setActiveItem(index);
+        setShowSort(false)
+    }
 
 
     return (
@@ -29,6 +36,7 @@ const Sort = () => {
                     viewBox="0 0 10 6"
                     fill="none"
                     xmlns="http://www.w3.org/2000/svg"
+                    className={showSort ? 'rotated' : ''}
                 >
                     <path
                         d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
@@ -36,15 +44,19 @@ const Sort = () => {
                     />
                 </svg>
                 <b>Сортировка по:</b>
-                <span onClick={toggleSortPopup}>популярности</span>
+                <span onClick={toggleSortPopup}>{activeLabel}</span>
             </div>
             {
                 showSort &&
                 <div className="sort__popup">
                     <ul>
-                        <li className="active">популярности</li>
-                        <li>цене</li>
-                        <li>алфавиту</li>
+                        {items.map((item, index) => {
+                            return <li
+                                className={activeItem === index ? 'active' : ''}
+                                key={`${item}_${index}`}
+                                onClick={() => {onSelectItem(index)}}
+                            >{item}</li>
+                        })}
                     </ul>
                 </div>
             }
