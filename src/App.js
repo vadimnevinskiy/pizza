66b3b6408acee1bbca33e1c16e5a94426d81f96e
Vadim Nevinskiy <vadim_nevinskiy@mail.ru>
2021-05-10 16:48:56 +0500

    Move sort block to Sort.jsx and created click on Document

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import 'materialize-css'
 import './scss/app.scss';
 
-import {Header, Categories} from "./components";
+import {Header, Categories, Sort} from "./components";
 
 
 function App() {
@@ -16,31 +16,7 @@ function App() {
                             onClickItem={(name) => console.log(name)}
                             items={['Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']}
                         />
-                        <div className="sort">
-                            <div className="sort__label">
-                                <svg
-                                    width="10"
-                                    height="6"
-                                    viewBox="0 0 10 6"
-                                    fill="none"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path
-                                        d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
-                                        fill="#2C2C2C"
-                                    />
-                                </svg>
-                                <b>Сортировка по:</b>
-                                <span>популярности</span>
-                            </div>
-                            <div className="sort__popup">
-                                <ul>
-                                    <li className="active">популярности</li>
-                                    <li>цене</li>
-                                    <li>алфавиту</li>
-                                </ul>
-                            </div>
-                        </div>
+                        <Sort />
                     </div>
                     <h2 className="content__title">Все пиццы</h2>
                     <div className="content__items">
